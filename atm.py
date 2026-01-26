dde7e8016e748b2b6cb0f5bc6b6097e675046e54
Jannis Mainczyk <jmainczyk@gmail.com>
2026-01-26 12:20:02 +0100

    custom coin values

--- atm.py
+++ atm.py
@@ -1,46 +1,72 @@
-from collections import defaultdict
+from collections import Counter, defaultdict
 
 import pytest
 
-COINS = [200, 100, 50, 20, 10, 5, 2, 1]
 
+class ATM:
+    standard_coins = [200, 100, 50, 20, 10, 5, 2, 1]
 
-def get_minimal_coins(total_amount_in_cents: int) -> dict[int, int]:
-    if total_amount_in_cents < 0:
-        raise ValueError("only positive amounts are supported")
+    def __init__(self, coins: list[int] = standard_coins):
+        self.coins = coins
 
-    returned_coins = defaultdict(lambda: 0)
-    remaining_amount = total_amount_in_cents
+    def get_minimal_coins(self, total_amount_in_cents: int) -> dict[int, int]:
+        if total_amount_in_cents < 0:
+            raise ValueError("only positive amounts are supported")
 
-    for coin in COINS:
-        while remaining_amount >= coin:
-            returned_coins[coin] += 1
-            remaining_amount -= coin
+        returned_coins = defaultdict(lambda: 0)
+        remaining_amount = total_amount_in_cents
 
-    return returned_coins
+        for coin in self.coins:
+            while remaining_amount >= coin:
+                returned_coins[coin] += 1
+                remaining_amount -= coin
+
+        return returned_coins
 
 
 def test_should_get_no_coins_for_zero_amount():
-    assert get_minimal_coins(0) == {}, "no money is returned"
+    assert ATM().get_minimal_coins(0) == {}, "no money is returned"
 
 
 def test_should_raise_error_when_negative_value_is_given():
     with pytest.raises(ValueError):
-        get_minimal_coins(-1)
+        ATM().get_minimal_coins(-1)
 
 
 def test_should_get_single_coin_when_coin_with_that_value_exists():
-    for amount in COINS:
-        assert get_minimal_coins(amount) == {amount: 1}, (
+    for amount in ATM.standard_coins:
+        assert ATM().get_minimal_coins(amount) == {amount: 1}, (
             f"{amount} should return a single coin"
         )
 
 
 def test_should_return_multiple_coins_with_different_values():
-    result = get_minimal_coins(3)
+    result = ATM().get_minimal_coins(3)
     assert len(result) == 2
     assert result == {1: 1, 2: 1}
 
 
 def test_should_return_multiple_coins_with_same_value_when_required():
-    assert get_minimal_coins(4) == {2: 2}
+    # assert get_minimal_coins(4) == {2: 2}
+    assert_get_minimal_coins_returns([2, 2], 4)
+
+
+def test_should_support_custom_coin_sets():
+    assert ATM([100, 33, 1]).get_minimal_coins(133) == {100: 1, 33: 1}
+
+
+def assert_get_minimal_coins_returns(expected_coins: list[int], amount_in_cents: int):
+    expected_coins_dict = Counter(expected_coins)
+    result = ATM().get_minimal_coins(amount_in_cents)
+    assert result == expected_coins_dict
+
+
+if __name__ == "__main__":
+    import sys
+
+    total_value = int(sys.argv[1])
+    coins = dict(ATM().get_minimal_coins(total_value))
+    [
+        print((f"{k:2d} Cent" if k < 100 else f"{k // 100:2d} Euro") + f": {v}")
+        for k, v in coins.items()
+    ]
